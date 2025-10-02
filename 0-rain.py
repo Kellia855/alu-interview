0e2821feb9012eaf72bdd9d7cc761b2fb3cd60a0
Kellia855 <k.kamikazi@alustudent.com>
2025-10-02 23:18:07 +0200

    rain

---
+++ 0-rain.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def rain(walls):
+    """
+    Calculate how many square units of water will be retained.
+
+    Args:
+        walls (list): List of non-negative integers.
+
+    Returns:
+        int: Total amount of retained rainwater.
+    """
+    if not walls or len(walls) < 3:
+        return 0
+
+    left, right = 0, len(walls) - 1
+    left_max, right_max = walls[left], walls[right]
+    water = 0
+
+    while left < right:
+        if left_max <= right_max:
+            left += 1
+            left_max = max(left_max, walls[left])
+            water += left_max - walls[left]
+        else:
+            right -= 1
+            right_max = max(right_max, walls[right])
+            water += right_max - walls[right]
+
+    return water
